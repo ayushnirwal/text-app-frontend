06698cb3cf32d50a7303c87156ea5ed54a62bb62
coding_carrots20 <coding.carrots@gmail.com>
2021-05-23 20:36:43 +0530

    state persistance added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import CounterReducer from "./counter/counterSlice";
 import UserReducer from "./user/userSlice";
 import ThemeReducer from "./theme/themeSlice";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const reducers = combineReducers({
+  counter: CounterReducer,
+  user: UserReducer,
+  theme: ThemeReducer,
+});
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-  reducer: { counter: CounterReducer, user: UserReducer, theme: ThemeReducer },
+  reducer: persistedReducer,
   middleware: [thunk, logger],
 });
