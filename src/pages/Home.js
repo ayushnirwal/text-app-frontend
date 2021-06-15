3323a48c1b503a25dc5fd687c4326b162cc40353
coding_carrots20 <coding.carrots@gmail.com>
2021-06-15 16:45:35 +0530

    feature: more addition home scrren

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useHistory } from "react-router";
+import ChatBox from "../components/Home/ChatBox/ChatBox";
 import Friends from "../components/Home/Friends";
 import Info from "../components/Home/Info";
 import MobileSideBarUI from "../components/Home/MobileSideBarUI";
@@ -15,12 +16,15 @@ const Home = () => {
   const { theme } = useTheme();
   return (
     <div className="w-screen h-screen bg-darkGray flex items-center justify-center">
-      <div className=" w-full h-full md:w-10/12 md:h-5/6 md:border-cream md:border-2 rounded-lg ">
+      <div className=" w-full h-full md:w-10/12 md:h-5/6 md:border-cream md:border-2 rounded-lg flex flex-col md:grid md:grid-cols-12 md:grid-rows-6">
         <div className="md:hidden">
           <MobileSideBarUI />
         </div>
-        <div className="hidden md:block relative w-3/12 h-full md:border-cream border-r-2">
+        <div className="hidden md:block relative h-full md:border-cream border-r-2 md:col-span-3 md:row-span-6">
           <SideBar />
+        </div>
+        <div className="flex-1 px-5 pt-5 pb-20 md:p-0 md:col-span-9 md:row-span-6">
+          <ChatBox />
         </div>
       </div>
     </div>
