a97efbfd747dfe41d39db24a0e25a91380b3920c
coding_carrots20 <coding.carrots@gmail.com>
2021-06-22 10:03:42 +0530

    feature: merged newUI branch

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Settings from "./pages/Settings";
+import Settings from "./pages/settings";
+import ProtectedRoute from "./components/misc/ProtectedRoute";
 
 export default function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/login" component={Login} />
-        <Route exact path="/settings" component={Settings} />
-        <Route exact path="/" component={Home} />
+        <ProtectedRoute exact path="/settings" component={Settings} />
+        <ProtectedRoute exact path="/" component={Home} />
       </Switch>
     </Router>
   );
